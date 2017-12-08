fb37d087559b62759eb3324ab7c9f29d46213beb
Stian Hassel Øveråsen <stian@dekode.no>
2017-12-08 19:20:24 +0100

    Merge conflict cleanup

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -171,20 +171,3 @@ function hogan_args_as_string() : string {
 
 	return implode( ' ', array_filter( $classes ) );
 }
-
-/**
- * Conditionally join classnames together
- */
-function hogan_classnames() {
-	$args    = func_get_args();
-	$classes = array_map( function( $arg ) {
-		if ( is_array( $arg ) ) {
-			return implode( ' ', array_filter( array_map( function( $expression, $class ) {
-				return $expression ? $class : false;
-			}, $arg, array_keys( $arg ) ) ) );
-		}
-		return $arg;
-	}, $args );
-
-	return implode( ' ', array_filter( $classes ) );
-}
